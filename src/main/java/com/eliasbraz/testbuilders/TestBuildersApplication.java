66e23bf195ae346b8d12aa5104940d3463c3a19b
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-03-20 21:50:58 -0300

    Initial commit

---
+++ src/main/java/com/eliasbraz/testbuilders/TestBuildersApplication.java
@@ -0,0 +1,13 @@
+package com.eliasbraz.testbuilders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBuildersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBuildersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eliasbraz/testbuilders/TestBuildersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliasbraz.testbuilders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBuildersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
